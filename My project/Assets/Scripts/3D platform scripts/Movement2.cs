42130448f208348b4a8ed8c7e749646ba8dc6412
Correa1310 <146136159+Correa1310@users.noreply.github.com>
2024-02-26 14:15:25 -0800

    Updating animation

--- My project/Assets/Scripts/3D platform scripts/Movement2.cs
+++ My project/Assets/Scripts/3D platform scripts/Movement2.cs
@@ -5,62 +5,47 @@ using UnityEngine;
 
 public class Movement2 : MonoBehaviour
 {
-   public int score = 0;
-    public float Speed = 10f;
-    public float JumpForce = 10f;
-    public float GravityModifier = 1f;
-    public float OutOfBounds = -10f;
+   public float turnSpeed = 20f;
+    public float moveSpeed = 1f;
+    public float jumpForce = 3f;
     public bool IsOnGround = true;
-    private float _horizontalInput;
-    private float _forwardInput;
-    private bool _isAtCheckpoint = false;
-    private Vector3 _startingPosition;
-    private Vector3 _checkpointPosition;
-    private Rigidbody _playerRigidbody;
-    public bool IsPlayerOnGround; 
+    Vector3 m_Movement;
+    Rigidbody m_Rigidbody;
+    Quaternion m_Rotation = Quaternion.identity;
 
     // Start is called before the first frame update
     void Start()
     {
-        _playerRigidbody = GetComponent<Rigidbody>();
-        Physics.gravity *= GravityModifier;
-        _startingPosition = transform.position;
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
 
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && IsOnGround)
+        {
+            m_Rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            IsOnGround = false;
+        }
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        _horizontalInput = Input.GetAxis("Horizontal");
-        _forwardInput = Input.GetAxis("Vertical");
-
-        if(Input.GetKeyDown(KeyCode.Space) && IsOnGround)
-        {
-            _playerRigidbody.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
-            IsOnGround = false;
-        }
-
-        if(transform.position.y < OutOfBounds)
-        {
-            if(_isAtCheckpoint)
-            {
-                transform.position = _checkpointPosition;
-            }
-            else
-            {
-                transform.position = _startingPosition;
-            }
-        }
-    }
-
     void FixedUpdate()
     {
-        Vector3 movement = new Vector3(_horizontalInput, 0.0f, _forwardInput);
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
 
-        _playerRigidbody.AddForce(movement * Speed);
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize();
 
-         
-        
+        bool hasHorizontalInput = !Mathf.Approximately (horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately (vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+
+        Vector3 desiredForward = Vector3.RotateTowards (transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation (desiredForward);
+
+        m_Rigidbody.MovePosition (m_Rigidbody.position + m_Movement * moveSpeed * Time.deltaTime);
+        m_Rigidbody.MoveRotation (m_Rotation);
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -69,28 +54,10 @@ public class Movement2 : MonoBehaviour
         {
             IsOnGround = true;
         }
-
-       
     }
-    void OnTriggerEnter(Collider other)
+
+    public bool IsPlayerOnGround()
     {
-        if(other.gameObject.CompareTag("Checkpoint"))
-        {
-            _isAtCheckpoint = true;
-            _checkpointPosition = other.gameObject.transform.position;
-        }
-
-        if(other.gameObject.CompareTag("Collectible"))
-        {
-            score++;
-         
-            Destroy(other.gameObject);
-        }
-
-       
-
-    
-   
+        return IsOnGround;
     }
-
 }
